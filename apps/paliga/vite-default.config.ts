083d63615a9deb19d6caf26bc0a0f5b87bc52551
carrot-farm <chohoki@gmail.com>
2025-01-15 00:04:16 +0900

    fix: 타입 오류 수정

--- apps/paliga/vite-default.config.ts
+++ apps/paliga/vite-default.config.ts
@@ -81,7 +81,11 @@ export const defaultConfig: UserConfig = {
       afterBuild: () => {
         const srcPath = resolve(__dirname, "src/types.d.ts");
         const destPath = resolve(__dirname, "dist/types.d.ts");
+        const srcCss = resolve(__dirname, "src/react/DevTool/dev-tool.css");
+        const destCss = resolve(__dirname, "dist/dev-tool.css");
+
         copyFileSync(srcPath, destPath);
+        copyFileSync(srcCss, destCss);
       },
     }),
     /** tsconfig 파일에서 baseUrl, paths 등을 참조할 수 있도록 설정 */
